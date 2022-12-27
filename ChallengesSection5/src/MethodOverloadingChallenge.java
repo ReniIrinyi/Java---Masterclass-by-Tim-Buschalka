12d27adc4e4c02d85d3e7cdc8161d0edc735e7d7
Renáta Irinyi <103267773+ReniIrinyi@users.noreply.github.com>
2022-12-27 23:18:55 +0100

    2. commit

--- ChallengesSection5/src/MethodOverloadingChallenge.java
+++ ChallengesSection5/src/MethodOverloadingChallenge.java
@@ -1,2 +1,16 @@
 public class MethodOverloadingChallenge {
+    public static void main(String[] args) {
+        System.out.println( convertToCentimeters(5, 8) +"cm");
+    }
+
+    public static double convertToCentimeters(int hinches) {
+        return hinches * 2.54;
+    };
+
+    public static double convertToCentimeters(int hfeet, int rehinch) {
+        return  convertToCentimeters((hfeet*12)+rehinch) ;
+
+    }
+
+    ;
 }
---
+++ ChallengesSection5/src/SecondsAndMinutesChallenge.java
@@ -0,0 +1,12 @@
+public class SecondsAndMinutesChallenge {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getDurationString(int seconds){
+        return seconds;
+    };
+    public static int getDurationString(int minutes, int seconds){
+        return minutes;
+    };
+}
