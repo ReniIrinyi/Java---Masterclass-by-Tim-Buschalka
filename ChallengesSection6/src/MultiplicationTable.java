902311104b28f4dfa1eb30c9a1bccf0618f33534
reniirinyi <reniirinyi@icloud.com>
2023-01-03 22:58:29 +0100

    sum bugs fixed

---
+++ ChallengesSection6/src/MultiplicationTable.java
@@ -0,0 +1,25 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        multiplicationTable(3);
+    }
+
+    public static void multiplicationTable(int max) {
+        int number = 1;
+
+        while (number <= max) {
+            printMultiplicationTableRow(number, max);
+            number++;
+        }
+    }
+
+    public static void printMultiplicationTableRow(int number, int coefficient) {
+
+        int printable = number;
+        while (printable <= number * coefficient) {
+            System.out.print("  " + printable);
+            printable += number;
+        }
+
+        System.out.println("");
+    }
+}
--- ChallengesSection6/src/PaintJob.java
+++ ChallengesSection6/src/PaintJob.java
@@ -2,4 +2,16 @@ public class PaintJob {
     public static void main(String[] args) {
 
     }
+
+    // implement the sum method here again
+    public static int sum(int number1, int number2, int number3, int number4) {
+        // write some code here
+        return number1+number2+number3+number4;
+    }
+
+    public static double average(int number1, int number2, int number3, int number4) {
+        int sum=sum(number1, number2, number3, number4);
+        return -1;
+    }
+
 }
