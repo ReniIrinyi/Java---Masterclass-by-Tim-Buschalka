2519c73270988949786841c0fa980a9d93ce6eff
RenataIrinyi <reniirinyi@icloud.com>
2023-02-24 22:03:54 +0100

    TagImmerNeunChallenge

--- ChallengesSection9/src/TagImmerNeunChallenge.java
+++ ChallengesSection9/src/TagImmerNeunChallenge.java
@@ -1,30 +1,30 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class TagImmerNeunChallenge {
     public static void main(String[] args) {
-        System.out.println("Bitte gebe dein Gebrutstag an!");
+    /*    System.out.println("Bitte gebe dein Geburtstag an!");
         Scanner scan=new Scanner(System.in);
         String input=scan.nextLine();
         String[] inputArray=input.replace(",","").split("");
 
-        int [] newArray=new int[inputArray.length];
-        int [] diffArray=new int[inputArray.length/2];
+        int [] newArray=new int[inputArray.length-1];
+        int [] diffArray=new int[inputArray.length-1];
 
         for(int i=0; i<inputArray.length; i++){
             newArray[i]= Integer.parseInt(inputArray[i].trim());
         }
-        int number=0;
-        int counter=0;
+*/
+        int[] newArray={0,5,1,9,9,9};
+        int[] diffArray=new int[newArray.length-1];
 
+        int number=0;
         Arrays.sort(newArray);
         for(int j=0; j<diffArray.length; j++){
-            diffArray[j]=(newArray[counter+1]-newArray[counter]);
-            counter=counter+2;
+            diffArray[j]=Math.abs(newArray[j+1]-newArray[j]);
             number+=diffArray[j];
         }
 
-        System.out.println("Dein Geburtstag= " +Arrays.toString(inputArray));
+       // System.out.println("Dein Geburtstag= " +Arrays.toString(inputArray));
         System.out.println(Arrays.toString(newArray));
         System.out.println(Arrays.toString(diffArray));
         System.out.printf("Dein Promi-Faktor ist= %s ", number);
